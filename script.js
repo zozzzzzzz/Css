c5e704b8dde8fd2a382c8cb91aae9c61ec49ad4a
Ziad Mohamed Hassan Mohamed <127157199+zozzzzzzz@users.noreply.github.com>
2023-03-19 00:16:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function viewLogin() {
+    var myOverlay = document.getElementById("myOverlay");
+    myOverlay.style.display = "block";
+}
+
+function closeSearch() {
+    var myOverlay = document.getElementById("myOverlay");
+    myOverlay.style.display = "none";
+}
+
+
+function openNav(){
+    let mySidenav = document.getElementById("mySidenav")
+    mySidenav.style.width = "20%"
+}
+
+function closeNav(){
+    let mySidenav = document.getElementById("mySidenav")
+    mySidenav.style.width = "0"
+}
